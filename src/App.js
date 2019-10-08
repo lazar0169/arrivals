0bc177ebe7ac9fa051bbe7641271ab21b0f073a9
lazar0169 <lazar0169@gmail.com>
2019-10-08 15:09:06 +0200

    Sign in: started

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 import HomePage from './pages/homepage/homepage.component.jsx';
 import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component';
+import Sign from './pages/sign/sign.components';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Header />
         <Route exact path="/" component={HomePage} />
         <Route path="/shop" component={ShopPage} />
+        <Route path="/sign" component={Sign} />
       </div>
     </Router>
   );
